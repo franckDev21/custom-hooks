5f3c8fec76da03e1aab013bd3166f61693e12079
franckDev21 <82069062+franckDev21@users.noreply.github.com>
2023-04-28 08:28:31 +0100

    Update useModal.tsx
    update: add new function

--- useModal.tsx
+++ useModal.tsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 
 const useModal = () => {
   const [show, setShow] = useState(false);
+  const [value,setValue] = useState(0)
 
   const closeModal = () => setShow(false);
   const openModal = () => setShow(true);
 
+  const doAction = (val: any, callback: (val: any) => void) => {
+    callback(value);
+    setValue(val)
+  };
+
   return {
     show,
     openModal,
-    closeModal
+    closeModal,
+    doAction,
+    value
   }
 };
 
